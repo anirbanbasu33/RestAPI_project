5518094a98bf92b862a432924a0dfcf1fc25e74b
anirbanbasu <anirbanbasu33@gmail.com>
2025-06-12 12:51:44 +0530

    Flask App server is running

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-# Nothing yet
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "REST API is LIVE"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    #  * Running on http://127.0.0.1:5000
